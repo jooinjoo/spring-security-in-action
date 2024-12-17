3d0903bcdcdc27be99cd0e9078c07cc267bffff9
jooinjoo <jooinjoo0@gmail.com>
2024-12-18 02:03:04 +0900

    feat: 다른 방법으로 구성 설정

--- src/main/java/com/example/ssiach2ex1/config/ProjectConfig.java
+++ src/main/java/com/example/ssiach2ex1/config/ProjectConfig.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,11 +12,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectConfig {
 
     @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        var user = User.withUsername("john")
+                .password("{noop}12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(req -> req
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .build();
     }
 }
