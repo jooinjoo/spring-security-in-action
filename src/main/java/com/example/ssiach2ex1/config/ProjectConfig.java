0845ff771da22bd8181cc2a859ae29f0af533337
jooinjoo <jooinjoo0@gmail.com>
2024-12-18 01:54:51 +0900

    feat: 엔드포인트 권한 부여 구성 재정의

--- src/main/java/com/example/ssiach2ex1/config/ProjectConfig.java
+++ src/main/java/com/example/ssiach2ex1/config/ProjectConfig.java
@@ -2,30 +2,24 @@ package com.example.ssiach2ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(req -> req
+                        .anyRequest().permitAll())
                 .build();
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
