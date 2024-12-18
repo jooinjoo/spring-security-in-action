a6ac913fe5c9804a5c2639b1b3f5dd0369743a43
jooinjoo <jooinjoo0@gmail.com>
2024-12-18 15:19:10 +0900

    feat: 빌더를 이용해 UserDetails 형식의 인스턴스 만들기

--- src/main/java/com/example/ssiach2ex1/config/UserManagementConfig.java
+++ src/main/java/com/example/ssiach2ex1/config/UserManagementConfig.java
@@ -2,6 +2,7 @@ package com.example.ssiach2ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,13 @@ public class UserManagementConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        var user = User.withUsername("john")
-                // {noop}를 붙여, Spring Security가 내부적으로 NoOpPasswordEncoder를 암묵적으로 사용해, PasswordEncoder를 빈 등록 필요가 없음.
-                // 만약 {noop}를 뺀다면, java.lang.IllegalArgumentException 발생.
+        UserDetails u = User.withUsername("bill")
                 .password("{noop}12345")
-                .authorities("read")
+                .authorities("read", "write")
+                .accountExpired(false)
+                .disabled(true) // false로 바꿔주면 요청 가능
                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+        return new InMemoryUserDetailsManager(u);
     }
 }
