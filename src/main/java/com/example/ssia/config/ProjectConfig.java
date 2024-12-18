684c6884338df426a04a5c5d6a0798bddd8dd92b
jooinjoo <jooinjoo0@gmail.com>
2024-12-19 00:51:42 +0900

    feat: 3.3.3 UserDetailsManager 계약 구현

---
+++ src/main/java/com/example/ssia/config/ProjectConfig.java
@@ -0,0 +1,30 @@
+package com.example.ssia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String userByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(userByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
