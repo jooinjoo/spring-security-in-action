995bbdf87ca576cd680568ca4722232018bab472
jooinjoo <jooinjoo0@gmail.com>
2024-12-20 00:45:42 +0900

    feat: 4.1.4 DelegatingPasswordEncoder를 이용한 여러 인코딩 전략
    - bcrypt를 기본 인코더로 가지는 인스턴스 구현

--- src/main/java/com/example/ssia/config/ProjectConfig.java
+++ src/main/java/com/example/ssia/config/ProjectConfig.java
@@ -2,10 +2,17 @@ package com.example.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ProjectConfig {
@@ -19,5 +26,16 @@ public class ProjectConfig {
         userDetailsManager.setUsersByUsernameQuery(userByUsernameQuery);
         userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
         return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder(16384, 8, 1, 32, 16)); // 시큐리티 5.8 이상부터 기본 생성자 제거. 명확성과 사용자 정의 강화하기 위해 파라미터 필수.
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 }
