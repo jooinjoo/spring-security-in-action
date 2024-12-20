186554cc4653e5a2b9580c264a826b5da6e9b056
jooinjoo <jooinjoo0@gmail.com>
2024-12-20 20:30:41 +0900

    feat: SecurityContext 이용
    - 5.2.2 비동기 호출을 위한 보유 전략 이용

--- src/main/java/com/example/ssia/config/ProjectConfig.java
+++ src/main/java/com/example/ssia/config/ProjectConfig.java
@@ -1,45 +1,19 @@
 package com.example.ssia.config;
 
-import com.example.ssia.security.CustomAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
+@EnableAsync
 public class ProjectConfig {
 
-    @Autowired
-    @Lazy
-    private AuthenticationProvider authenticationProvider;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authenticationProvider(authenticationProvider) // Security 5.7 이전 configure 메서드를 대체.
-                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
-                .httpBasic(withDefaults())
-                .build();
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(
+                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
 }
