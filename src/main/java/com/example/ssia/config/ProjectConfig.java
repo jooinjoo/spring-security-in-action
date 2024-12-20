613472037784bc98d2a05b62f0c21efb4a3510dc
jooinjoo <jooinjoo0@gmail.com>
2024-12-20 23:27:59 +0900

    feat: HTTP Basic 인증과 양식 기반 로그인 인증 이해하기 - 5.3.1 HTTP Basic 이용 및 구성

--- src/main/java/com/example/ssia/config/ProjectConfig.java
+++ src/main/java/com/example/ssia/config/ProjectConfig.java
@@ -1,19 +1,25 @@
 package com.example.ssia.config;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableAsync
 public class ProjectConfig {
 
-//    @Bean
-//    public InitializingBean initializingBean() {
-//        return () -> SecurityContextHolder.setStrategyName(
-//                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
-//    }
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(c -> {
+                    c.realmName("OTHER");
+                    c.authenticationEntryPoint(new CustomEntryPoint());
+                });
+        return http.build();
+    }
 }
--- src/main/java/com/example/ssia/controllers/HelloController.java
+++ src/main/java/com/example/ssia/controllers/HelloController.java
@@ -1,17 +1,8 @@
 package com.example.ssia.controllers;
 
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
-import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -19,46 +10,5 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(Authentication authentication) {
         return "Hello!" + authentication.getName() + "!";
-    }
-
-    @GetMapping("/bye")
-    @Async
-    public void goodbye() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        String username = context.getAuthentication().getName();
-    }
-
-    @GetMapping("/ciao")
-    public String ciao() throws Exception {
-        // 현재 보안 컨텍스트에서 사용자 이름을 반환하는 Callable 작업 객체 선언
-        Callable<String> task = () -> {
-            SecurityContext context = SecurityContextHolder.getContext();
-            return context.getAuthentication().getName();
-        };
-
-        ExecutorService e = Executors.newCachedThreadPool();
-        try {
-            var contextTask = new DelegatingSecurityContextCallable<>(task);    // 새로 생성된 스레드에 현재 보안 컨텍스트를 제공
-            return "Ciao, " + e.submit(contextTask).get() + "!";
-        } finally {
-            e.shutdown();
-        }
-    }
-
-    @GetMapping("/hola")
-    public String hola() throws Exception {
-        Callable<String> task = () -> {
-            SecurityContext context = SecurityContextHolder.getContext();
-            return context.getAuthentication().getName();
-        };
-        ExecutorService e = Executors.newCachedThreadPool();
-
-        // DelegatingSecurityContextExecutorService가 ExecutorService를 장식해 작업을 제출할 때, 보안 컨텍스트 세부 정보를 전파
-        e = new DelegatingSecurityContextExecutorService(e);
-        try {
-            return "Hola, " + e.submit(task).get() + "!";
-        } finally {
-            e.shutdown();
-        }
     }
 }
